ecf2ef4d2c89e038f017603a6f8a588ac1bdd5ca
Umar Farooq <uf29664@gmail.com>
2024-06-08 20:22:42 -0700

    school project of navbar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 import Image from 'next/image'
-
+import Navbar from './components/navbar/Navbar'
+import bgImage from './../public/image 12.png'
 export default function Home() {
   return (
     <div className="">
+      <div className="" style={bgImage} >
+
+      <Navbar/>
+      </div>
       
     </div>
   )
