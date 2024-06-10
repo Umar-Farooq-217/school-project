344a636584359144258b94375fb8b1f512159421
Umar Farooq <uf29664@gmail.com>
2024-06-10 05:50:29 -0700

    school website

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import navImage from '../../../public/image 12 '
 export default function Navbar() {
   return (
     <div>
+      <div className="">
+        <Image scr={} />
+      </div>
+
       
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import Navbar from './components/navbar/Navbar'
 import bgImage from './../public/image 12.png'
+import './globals.css'
 export default function Home() {
   return (
     <div className="">
-      <div className="" style={bgImage} >
+      <div className="background "  >
 
       <Navbar/>
       </div>
